57f26fe092588715f865f31e67db102c295a8024
vuquang23 <levuquang2k1@gmail.com>
2022-11-18 00:04:36 +0700

    refactor code

--- internal/pkg/services/spot_trade_impl.go
+++ internal/pkg/services/spot_trade_impl.go
@@ -65,7 +65,7 @@ func (s *spotTradeService) GetTradingPairsInfo(ctx context.Context) ([]*en.SpotT
 			info.TotalUnitBought = totalUnitBought
 		}
 
-		if price, err := b.GetSpotPrice(s.bclient, w.Symbol); err != nil {
+		if price, err := b.GetSpotPrice(ctx, s.bclient, w.Symbol); err != nil {
 			return nil, err
 		} else {
 			info.QuoteAmount = info.UnitNotional * (float64(info.UnitBuyAllowed) - float64(info.TotalUnitBought))
