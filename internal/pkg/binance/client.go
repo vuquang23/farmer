8a0a370c173c5b8efdbcab5bd755d2564860ed5e
vuquang23 <levuquang2k1@gmail.com>
2022-12-04 17:42:18 +0700

    add info usdt balance

--- internal/pkg/binance/client.go
+++ internal/pkg/binance/client.go
@@ -1,10 +1,14 @@
 package binance
 
 import (
+	goctx "context"
+
 	"github.com/adshao/go-binance/v2"
 	"github.com/adshao/go-binance/v2/futures"
 
 	"farmer/internal/pkg/config"
+	"farmer/internal/pkg/utils/context"
+	"farmer/internal/pkg/utils/logger"
 )
 
 var spot *binance.Client
@@ -16,6 +20,22 @@ func InitBinanceSpotClient(isTest bool) {
 		}
 		cfg := config.Instance().Binance
 		spot = binance.NewClient(cfg.ApiKey, cfg.SecretKey)
+
+		spotNotifyCurrentUSDTBalance()
+	}
+}
+
+func spotNotifyCurrentUSDTBalance() {
+	ctx := context.Child(goctx.Background(), "[spot-notify-current-usdt-balance]")
+	data, err := spot.NewGetAccountService().Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range data.Balances {
+		if d.Asset == "USDT" {
+			logger.Infof(ctx, "%+v", d)
+			break
+		}
 	}
 }
 
