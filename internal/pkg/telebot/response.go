0b7f8c3a1b4264ad9c1023bcacd4a58348607693
vuquang23 <levuquang2k1@gmail.com>
2023-01-05 15:54:50 +0700

    fix typo

--- internal/pkg/telebot/response.go
+++ internal/pkg/telebot/response.go
@@ -8,8 +8,8 @@ type (
 		CurrentUSDValue float64
 		BenefitUSD      float64
 		ChangedUSD      float64
-		BaseAmount      float64
-		QuoteAmount     float64
+		BaseQty         float64
+		QuoteQty        float64
 		UnitBuyAllowed  uint64
 		UnitNotional    float64
 		TotalUnitBought uint64
