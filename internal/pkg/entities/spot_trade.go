2b47a41faa30627848de818d473b448b1c289b51
Le Vu Quang <54746883+vuquang23@users.noreply.github.com>
2023-01-05 14:15:51 +0700

    fix (#31)

--- internal/pkg/entities/spot_trade.go
+++ internal/pkg/entities/spot_trade.go
@@ -3,17 +3,17 @@ package entities
 import "time"
 
 type SpotTrade struct {
-	ID                  uint64 `gorm:"primaryKey;autoIncrement"`
-	Symbol              string
-	Side                string
-	BinanceOrderID      uint64
-	SpotWorkerID        uint64
-	Qty                 string
-	CummulativeQuoteQty string
-	Price               float64
-	Ref                 uint64
-	IsDone              bool
-	UnitBought          uint64
-	CreatedAt           time.Time
-	UpdatedAt           time.Time
+	ID             uint64 `gorm:"primaryKey;autoIncrement"`
+	Symbol         string
+	Side           string
+	BinanceOrderID uint64
+	SpotWorkerID   uint64
+	Qty            string
+	QuoteQty       string
+	Price          float64
+	Ref            uint64
+	IsDone         bool
+	UnitBought     uint64
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
